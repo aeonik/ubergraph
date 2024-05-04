7f1cf39da6525966df2918282d25bfcca0efc480
Aeonik Chaos <aeonik.chaos@gmail.com>
2024-05-04 12:34:17 -0400

    FIX: /issues/63

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.priority-map "0.0.10"]
                  [aysylu/loom "1.0.2"]
-                 [dorothy "0.0.6"]
+                 [dorothy "0.0.7"]
                  [com.rpl/specter "1.1.2"]
                  [potemkin "0.4.5"]]
   :codox {:output-path "doc"
