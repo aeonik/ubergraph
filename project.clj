b728063fc0eda480f7181c3f9e23ab7a947bff65
Aeonik Chaos <aeonik.chaos@gmail.com>
2024-08-29 12:00:19 -0400

    Fully qualify dependencies

--- project.clj
+++ project.clj
@@ -6,9 +6,9 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.priority-map "0.0.10"]
                  [aysylu/loom "1.0.2"]
-                 [dorothy "0.0.6"]
-                 [com.rpl/specter "1.1.2"]
-                 [potemkin "0.4.5"]]
+                 [dororhy/dorothy "0.0.7"]
+                 [com.rpl/specter "1.1.4"]
+                 [potemkin/potemkin "0.4.5"]]
   :codox {:output-path "doc"
           :namespaces [ubergraph.core ubergraph.alg]
           :source-uri "http://github.com/Engelberg/ubergraph/tree/master/{filepath}#L{line}"}
